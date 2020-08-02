9637b5853b9b711d5b0f21057ce4416b9523a74e
cjwit <chris.witulski@gmail.com>
2020-08-02 15:06:51 -0400

    begin reorganizing routes and queries

---
+++ server/routes.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('../client/dist'));
+
+// database
+const db = require('./db');
+const { pool } = require('./db');
+
+// for sending data
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// sent page to client
+app.get('/', (req, res) => {
+  res.sendFile('index.html');
+});
+
+// queries
+const queryGetAllItems = 'SELECT * FROM items;'
+const queryPostTemplate = `
+DROP TABLE IF EXISTS items;
+
+CREATE TABLE items(
+  id varchar(30) PRIMARY KEY,
+  item_name varchar(50),
+  item_number integer
+);
+
+INSERT INTO items (id, item_name, item_number) VALUES`
+
+// create query from req.body
+var buildInsertQuery = function(itemList) {
+  var queryText = queryPostTemplate;
+  for (var i = 0; i < itemList.length; i++) {
+    let item = itemList[i];
+    let itemString = `\n  ('${item.id}', '${item.item_name}', ${item.item_number}),`
+    queryText += itemString;
+  }
+  queryText = queryText.replace(/,$/,";");
+  return queryText;
+}
+
+// get data from database and send to the client
+app.get('/storage', (req, res) => {
+  pool.query(queryGetAllItems, (err, results) => {
+    // console.log(" => getting items", results.rows)
+    res.send({ items: results.rows })
+  })
+});
+
+// save data on update from the client
+app.post('/storage', (req, res) => {
+  var queryText = buildInsertQuery(req.body);
+  pool.query(queryText, (err, res) => {
+    // console.log(res);
+    // res.status(201).send(' --> cleared table');
+  })
+})
+
+module.exports = app;
